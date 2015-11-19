67c6a0046a20267e95b09f1542cb24d478a2809a
gaojiasheng <gaojiasheng.him@foxmail.com>
2015-11-19 11:36:49 +0800

    pre代码加入overflow:auto

--- themes/hugo-uno/static/js/main.js
+++ themes/hugo-uno/static/js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
       $('.content-wrapper').addClass('animated slideInRight');
     } else {
       $('.panel-cover').css('max-width',currentWidth);
-      $('.panel-cover').animate({'max-width': '350px', 'width': '30%'}, 400, swing = 'swing', function() {} );
+      $('.panel-cover').animate({'max-width': '420px', 'width': '30%'}, 400, swing = 'swing', function() {} );
     }
   });
 
